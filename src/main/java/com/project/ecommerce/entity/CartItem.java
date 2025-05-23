ce23492fa27f695eb15eb6501f218b0581790544
gmldnjs99 <sonks9@naver.com>
2025-05-23 12:05:31 +0900

    장바구니(CartItem) 엔티티 추가
    - 사용자(User)와 상품(Product) 간 다대일 관계 매핑
    - 장바구니 상품 수량(quantity) 필드 포함

---
+++ src/main/java/com/project/ecommerce/entity/CartItem.java
@@ -0,0 +1,24 @@
+package com.project.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+}
